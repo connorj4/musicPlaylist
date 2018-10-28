da32c48cf63270a7c0c9ea4b3f96364ff20d7763
@joshua40 <josh@knockmedia.com>
2018-10-27 20:30:37 -0400

    1st commit add sign in files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # musicPlaylist
-music playlist
+A basic database that allows users to create playlists and add music, admins are allowed add, edit, search and remove entries from the database.
+##Stack
+The project was built using [xampp] (https://www.apachefriends.org/index.html)
+- Appache
+- MariaDB
+- PHP
+- Pearl
---
+++ template.php
@@ -0,0 +1,14 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <?php include_once "./include/head.php"; ?>
+  </head>
+  <body>
+    <?php include_once "./include/header.php"; ?>
+    <main role="main" class="container">
+      <!-- Primary Content Goes Here -->
+    </main>
+    <?php include_once "./include/footer.php"; ?>
+  </body>
+</html>
